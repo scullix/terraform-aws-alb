ec678ec5fb08a74a55c6a87454db46396bba705a
scullix <153437250+scullix@users.noreply.github.com>
2025-01-10 17:11:04 +0100

    Add Application Load Balancer configuration

---
+++ alb.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "app_lb" {
+  name               = "${var.tag_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+
+  tags = {
+    Name = "${var.tag_name}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.tag_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance_attachment" {
+  for_each         = aws_instance.my_app_eg1
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = each.value.id
+  port             = 80
+}
